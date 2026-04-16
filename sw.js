9a9bd7900e7ce21e705d622dd9e4e7745f39d068
pezzaliapp <pezzalialessandro@gmail.com>
2026-04-16 18:45:30 +0200

    hot cue CLEAR mode v1.3.2

--- sw.js
+++ sw.js
@@ -8,7 +8,7 @@
  *   notifica l'utente e skippa il waiting automaticamente
  */
 
-const APP_VERSION = 'djapp-v1.3.1'
+const APP_VERSION = 'djapp-v1.3.2'
 const CACHE_NAME = `${APP_VERSION}`
 
 // Asset da pre-cachare (vengono aggiornati ad ogni build)
