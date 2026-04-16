f13126f9a70893c4cb807956e9a3361c97c8982a
pezzaliapp <pezzalialessandro@gmail.com>
2026-04-16 21:57:46 +0200

    debug trace setTrack v1.7.2

--- sw.js
+++ sw.js
@@ -8,7 +8,7 @@
  *   notifica l'utente e skippa il waiting automaticamente
  */
 
-const APP_VERSION = 'djapp-v1.7.1'
+const APP_VERSION = 'djapp-v1.7.2'
 const CACHE_NAME = `${APP_VERSION}`
 
 // Asset da pre-cachare (vengono aggiornati ad ogni build)
