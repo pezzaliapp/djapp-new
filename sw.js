401f97bf050968194001df3c69b2241e5e94a55f
pezzaliapp <pezzalialessandro@gmail.com>
2026-04-16 22:19:33 +0200

    revert layout visivo v2.0.1

--- sw.js
+++ sw.js
@@ -8,7 +8,7 @@
  *   notifica l'utente e skippa il waiting automaticamente
  */
 
-const APP_VERSION = 'djapp-v2.0.0'
+const APP_VERSION = 'djapp-v2.0.1'
 const CACHE_NAME = `${APP_VERSION}`
 
 // Asset da pre-cachare (vengono aggiornati ad ogni build)
