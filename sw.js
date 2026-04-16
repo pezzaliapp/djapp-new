c416d826df36f34f560f9f48ed3116eae1f1b2dc
pezzaliapp <pezzalialessandro@gmail.com>
2026-04-16 22:40:31 +0200

    fix layout 3 colonne definitivo v2.0.6

--- sw.js
+++ sw.js
@@ -8,7 +8,7 @@
  *   notifica l'utente e skippa il waiting automaticamente
  */
 
-const APP_VERSION = 'djapp-v2.0.5'
+const APP_VERSION = 'djapp-v2.0.6'
 const CACHE_NAME = `${APP_VERSION}`
 
 // Asset da pre-cachare (vengono aggiornati ad ogni build)
