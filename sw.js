82a49f61a0e3a94836f7533431febccc9ec53154
pezzaliapp <pezzalialessandro@gmail.com>
2026-04-16 22:28:50 +0200

    ripristino CSS originale da transcript v2.0.4

--- sw.js
+++ sw.js
@@ -8,7 +8,7 @@
  *   notifica l'utente e skippa il waiting automaticamente
  */
 
-const APP_VERSION = 'djapp-v2.0.3'
+const APP_VERSION = 'djapp-v2.0.4'
 const CACHE_NAME = `${APP_VERSION}`
 
 // Asset da pre-cachare (vengono aggiornati ad ogni build)
